fb335bc587b393b785fc60aee712df805f23ed03
Jake0303 <jake0303@hotmail.ca>
2020-11-30 17:21:10 -0500

    Added libs

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-
+require('dotenv').config();
+var request = require('request');
+var accesstoken = process.env.accesstoken;
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
-//Colton Alert
-router.post('/coltonalert', function (req, res, next) {
-    console.log(req.body);
+function submitOrder(side, qty, symbol) {
     var orderObject = {
         "orderType": "MARKET",
         "session": "NORMAL",
@@ -17,31 +17,98 @@ router.post('/coltonalert', function (req, res, next) {
         "orderStrategyType": "SINGLE",
         "orderLegCollection": [
             {
-                "instruction": req.body.side.toUpperCase(),
-                "quantity": req.body.quantity,
+                "instruction": side,
+                "quantity": qty,
                 "instrument": {
-                    "symbol": req.body.symbol.toUpperCase(),
+                    "symbol": symbol,
                     "assetType": "EQUITY"
                 }
             }
         ]
     }
-    //Place Order
-    var placeorder_req = {
-        url: 'https://api.tdameritrade.com/v1/accounts/' + bot.lastAccountID + '/orders',
-        method: 'POST',
+    //Get main account id
+    var account_req = {
+        url: 'https://api.tdameritrade.com/v1/accounts',
+        method: 'GET',
         headers: {
-            'Authorization': 'Bearer ' + bot.accesstoken,
-            'content-type': 'application/json',
-            'connection': 'Keep-Alive'
-        },
-        body: orderObject,
-        json: true
-    };
-    request(placeorder_req, function (error, response, body) {
-        console.log(body);
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Bearer ' + accesstoken
+        }
+    }
+    request(account_req, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var accountId = body[0]['securitiesAccount']['accountId'];
+            //Place Order
+            var placeorder_req = {
+                url: 'https://api.tdameritrade.com/v1/accounts/' + accountId + '/orders',
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + accesstoken,
+                    'content-type': 'application/json',
+                    'connection': 'Keep-Alive'
+                },
+                body: orderObject,
+                json: true
+            };
+            request(placeorder_req, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                } else {
+                    resetAccessToken(function () {
+                        submitOrder(side, qty, symbol);
+                    });
+                }
+            });
+        } else {
+            resetAccessToken(function () {
+                submitOrder(side, qty, symbol);
+            });
+        }
     });
+}
+
+//Colton Alert
+router.post('/coltonalert', function (req, res, next) {
+    console.log(req.body);
+    submitOrder(req.body.side.toUpperCase(), req.body.quantity, req.body.symbol.toUpperCase());
     res.status(200).end() // Responding is important
 });
 
+
+function resetAccessToken(done) {
+    try {
+        var refreshtoken_req = {
+            url: 'https://api.tdameritrade.com/v1/oauth2/token',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            form: {
+                'grant_type': 'refresh_token',
+                'refresh_token': process.env.accesstoken,
+                'access_type': '',
+                'client_id': process.env.CLIENT_ID
+            }
+        };
+
+        request(refreshtoken_req, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log('Successfully reset access token.');
+                // get the TDA response
+                var authReply = JSON.parse(body);
+                accesstoken = authReply.access_token;
+                done();
+            } else {
+                console.log('Could not reset access token.');
+                console.log(body);
+                done()
+            }
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+resetAccessToken(function () {});
 module.exports = router;
